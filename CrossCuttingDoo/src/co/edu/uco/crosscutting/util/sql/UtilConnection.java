46f99e0447f2efa6ef5de35d4af68f411e8816be
Emmanuel Lopera <ejrocko@hotmail.com>
2022-05-06 21:32:50 -0500

    Actualizacion de grades 5/6/22

---
+++ CrossCuttingDoo/src/co/edu/uco/crosscutting/util/sql/UtilConnection.java
@@ -0,0 +1,26 @@
+package co.edu.uco.crosscutting.util.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import co.edu.uco.crosscutting.exception.GeneralException;
+import co.edu.uco.crosscutting.util.object.UtilObject;
+
+public class UtilConnection {
+    private UtilConnection() {
+
+    }
+
+    public static boolean isClosed(Connection connection) {
+
+        if (UtilObject.getUtilObject().isNull(connection)) {
+            throw GeneralException.build("connectio is null!!");
+        }
+        try {
+            return connection.isClosed();
+        } catch (SQLException exception) {
+            throw GeneralException.build("problems trying to validate if connection was closed!", exception);
+        }
+    }
+
+}
